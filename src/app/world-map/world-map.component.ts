0a582e432d91ed0de6df696434cbb424e89bb5fa
Mike Horn <v-mikehorn@microsoft.com>
2024-02-29 10:10:49 -0500

    Generated world-map Component and related files.

---
+++ src/app/world-map/world-map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-world-map',
+  templateUrl: './world-map.component.html',
+  styleUrl: './world-map.component.css'
+})
+export class WorldMapComponent {
+
+}
