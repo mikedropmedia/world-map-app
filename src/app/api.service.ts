74db7b2c74ba6d0008dcb3ca2dd9d17e767b87d8
Mike Horn <v-mikehorn@microsoft.com>
2024-03-02 12:25:48 -0500

    Create fetchCountryData function with api variable. Call WorldBank api URL to return JSON. Create mouseenter event to call setCountryData function to log fetchCountryData event targeting country id.

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,4 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
   constructor(private http: HttpClient) {}
+
+  fetchCountryData(country: string) {
+    let api = `http://api.worldbank.org/v2/country/all/?format=json`;
+    return this.http.get(api);
+  }
 }
--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-world-map',
   templateUrl: './world-map.component.html',
-  styleUrl: './world-map.component.css'
+  styleUrl: './world-map.component.css',
 })
 export class WorldMapComponent {
-
+  setCountryData(event: any) {
+    console.log('event', event.target.id);
+  }
 }
