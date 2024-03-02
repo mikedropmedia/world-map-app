19f84ec9f3f8ecc09530c5db30e1ff09ae0d8dd6
Mike Horn <v-mikehorn@microsoft.com>
2024-03-02 11:36:13 -0500

    Generated new "api" service. Import HttpClient in api.service.ts. Add import HttpClientModule from angular/common/http. Add import to @NgModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorldMapComponent } from './world-map/world-map.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WorldMapComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  ],
+  declarations: [AppComponent, WorldMapComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
