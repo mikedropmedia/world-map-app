cd1ac97310d8c026c3665ff81d49759b936fd732
Mike Horn <v-mikehorn@microsoft.com>
2024-03-05 11:57:49 -0500

    Updates to data displayed in html and fixed error in getCountryData method for response parameter.

--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -4,9 +4,8 @@ import { ApiService } from '../api.service';
 @Component({
   selector: 'app-world-map',
   templateUrl: './world-map.component.html',
-  styleUrl: './world-map.component.css',
+  styleUrls: ['./world-map.component.css'],
 })
-
 export class WorldMapComponent {
   countryData: any = {};
 
@@ -15,14 +14,26 @@ export class WorldMapComponent {
   getCountryData(event: MouseEvent) {
     const countryCode = (event.target as SVGElement).id;
     this.apiService.getCountryData(countryCode).subscribe({
-      next: (data) => {
-        this.countryData = data;
-        console.log(data); // You can now display this data in your template
+      next: (response) => {
+        this.processApiResponse(response);
+        console.log(response);
       },
       error: (error) => {
         console.error('Error fetching country data:', error);
-      }
+      },
     });
   }
-}
 
+  private processApiResponse(response: any) {
+    const data = response[1][0];
+
+    this.countryData = {
+      countryName: data.name,
+      capitalCity: data.capitalCity,
+      region: data.region.value,
+      incomeLevel: data.incomeLevel.value,
+      lendingType: data.lendingType.value,
+      iso2Code: data.iso2Code,
+    };
+  }
+}
